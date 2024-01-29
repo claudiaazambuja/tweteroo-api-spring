e0a3ad1bd9684cb8d6fe7831dfa69764a7fe9eef
Claudia Azambuja <cclabdis@gmail.com>
2024-01-29 00:40:30 -0300

    implementação da arquitetura básica

--- api/src/main/java/com/tweeteroo/api/controllers/TweetController.java
+++ api/src/main/java/com/tweeteroo/api/controllers/TweetController.java
@@ -1,5 +1,33 @@
 package com.tweeteroo.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/tweets")
 public class TweetController {
+
+    @PostMapping    
+    public  String create(@RequestBody String body){
+        System.out.println(body);
+        return body;
+    }
+
+    @GetMapping       
+    public  String findAll(){
+        return "Algum conteudo";
+    }
+
+    @GetMapping("user/{userId}")       
+    public  String findById(@PathVariable Long userId){
+        return "Algum conteudo" + userId;
+    }
     
 }
--- api/src/main/java/com/tweeteroo/api/controllers/UserController.java
+++ api/src/main/java/com/tweeteroo/api/controllers/UserController.java
@@ -1,5 +1,21 @@
 package com.tweeteroo.api.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
     
+    @PostMapping    
+    public  String create(@RequestBody String body){
+        System.out.println(body);
+        return body;
+    }
 }
+
+
+
+   
